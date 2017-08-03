1017a2db85eac3d9888aad1dbe520d8dc7c09390
ofthestorm <870771017@qq.com>
2017-08-03 19:59:01 +0800

    music

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function over() {
-    alert("h");
-    // document.getElementsByName("musicImg").src = "Images/No.png";
-    // document.getElementsByName("musicWord").innerHTML = "关闭背景音乐";
+    console.log(document.getElementById("musicWord").innerHTML);
+    if(document.getElementById("musicWord").innerHTML != "播放背景音乐") {
+        document.getElementById("musicImg").src = "images/No.png";
+        document.getElementById("musicWord").innerHTML = "关闭背景音乐";
+    }
+
+}
+
+function out() {
+    console.log(document.getElementById("musicWord").innerHTML);
+    if(document.getElementById("musicWord").innerHTML != "播放背景音乐")
+    document.getElementById("musicImg").src = "images/Unknown.gif";
+    document.getElementById("musicWord").innerHTML = "City of stars";
+}
+
+function clicked() {
+    console.log(document.getElementById("musicWord").innerHTML);
+    if(document.getElementById("musicWord").innerHTML === "关闭背景音乐") {
+        document.getElementById("musicImg").src = "images/stop.png";
+        document.getElementById("musicWord").innerHTML = "播放背景音乐";
+    } else {
+        document.getElementById("musicImg").src = "images/Unknown.gif";
+        document.getElementById("musicWord").innerHTML = "City of stars";
+    }
 }
